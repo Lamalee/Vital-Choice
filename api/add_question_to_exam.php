3ad0898e720b5785bea9370944888715e5ee65fe
Lamalee <lqd.tik23.lengoclam@gmail.com>
2026-05-27 16:37:51 +0700

    update Lam branch
    update Lam branch

--- api/add_question_to_exam.php
+++ api/add_question_to_exam.php
@@ -3,9 +3,9 @@ include '../profile/db.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $exam_id = $_POST['exam_id'] ?? null;
     $question_id = $_POST['question_id'] ?? null;
-
+    $question_tag = $_POST['question_tag'] ?? null;
     if ($exam_id && $question_id) {
-        $sql = "INSERT INTO exam_questions (exam_id, question_id) VALUES ('$exam_id', '$question_id')";
+        $sql = "INSERT INTO exam_questions (exam_id, question_id, tag) VALUES ('$exam_id', '$question_id', '$question_tag')";
         
         if ($conn->query($sql)) {
             echo json_encode(["success" => true]);
